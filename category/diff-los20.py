4469cfed93af3b63496a9b15ff6f1edd80050718
pr <qq952720215@126.com>
2026-07-05 21:25:23 +0800

    a

--- category/diff-los20.py
+++ category/diff-los20.py
@@ -20,8 +20,7 @@ baseurl = {
 gl = gitlab.Gitlab(configure.GITLAB_URL, private_token=configure.PRIVATE_TOKEN)
 
 
-def parse(xml_path, cb):
-    # 解析 XML 清单
+def init(xml_path):
     tree = ET.parse(xml_path)
     root = tree.getroot()
     for project in root.findall('remote'):
@@ -34,6 +33,12 @@ def parse(xml_path, cb):
         revision = project.get('revision')
         if revision is not None:
             revisions[name] = revision
+
+
+def parse(xml_path, cb):
+    # 解析 XML 清单
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
 
     # 遍历 xml 中的所有 project 标签
     for project in root.findall('project'):
@@ -51,8 +56,10 @@ def parse(xml_path, cb):
             if 1 and remote == "aosp":
                 cb(ppp, revision)
             if 0 and remote == "github":
-                if same_last_commit(name, revision, ppp):
+                if not same_last_commit(name, revision, ppp):
                     print(ppp)
+                    with open("/tmp/aosp/log/change.log", "a", encoding="utf-8") as f:
+                        f.write(ppp + "\n")
         except GitlabGetError as e:
             if e.response_code == 404:
                 add(ppp, baseurl[remote] + name, remote)
@@ -60,10 +67,7 @@ def parse(xml_path, cb):
 
 def same_last_commit(name, branch, path):
     if not branch.startswith("refs/heads/"):
-        if branch != "lineage-20.0":
-            raise Exception("branch", branch)
-
-    branch = branch.replace("refs/heads/", "")
+        branch = branch.replace("refs/heads/", "")
     project = gl.projects.get(name)
     b1 = project.branches.get(configure.SELF_TAG)
     b2 = project.branches.get(branch)
@@ -89,9 +93,11 @@ def aosp(path, revision):
     # print(head_commit, tag_commit)
     if 1 and head_commit != tag_commit:
         print(path)
-        with open("./log/change.log", "a", encoding="utf-8") as f:
+        with open("log/change.log", "a", encoding="utf-8") as f:
             f.write(path + "\n")
 
 
 if __name__ == "__main__":
+    init(configure.MANIFEST_PATH)
+    # parse(configure.LINEAGE, aosp)
     parse(configure.MANIFEST_PATH, aosp)
